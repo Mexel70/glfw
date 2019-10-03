2461ad690efceda5138d06b29465b66177acd8e3
Mexel70 <mexel70@live.de>
2019-10-03 17:04:05 +0200

    Apple builds

--- premake5.lua
+++ premake5.lua
@@ -10,14 +10,15 @@ project "glfw"
 	files {
         "premake5.lua",
         
-        "include/GLFW/**.h",
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
 
         "src/init.c",
         "src/context.c",
         "src/monitor.c",
-        "src/input.c",
         "src/window.c",
+        "src/input.c",
         "src/vulkan.c"
     }
     
@@ -30,20 +31,23 @@ project "glfw"
 
         files {
             "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
+
             "src/wgl_context.c",
             "src/egl_context.c",
             "src/osmesa_context.c"
+            
+            "src/win32_monitor.c",
+            "src/win32_window.c",
+            "src/win32_joystick.c",
+
+            "src/win32_time.c",
+            "src/win32_thread.c",
         }
 
-    defines { 
-        "_GLFW_WIN32",
-        "_CRT_SECURE_NO_WARNINGS"
-    }
+        defines { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
 
     
     filter "system:linux"
@@ -51,19 +55,51 @@ project "glfw"
 
         files {
             "src/x11_init.c",
-            "src/x11_monitor.c",
-            "src/x11_window.c",
-            "src/xkb_unicode.c",
-            "src/posix_time.c",
-            "src/posix_thread.c",
+
             "src/glx_context.c",
             "src/egl_context.c",
             "src/osmesa_context.c",
-            "src/linux_joystick.c"
+
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/linux_joystick.c",
+
+            "src/posix_thread.c",
+            "src/posix_time.c",
+            "src/xkb_unicode.c"
         }
 
         defines {
             "_GLFW_X11"
+        }
+
+    filter "system:macosx"
+        systemversion "latest"
+
+        files {
+            "src/nsgl_context.h",
+            "src/egl_context.h",
+            "src/osmesa_context.h",
+
+            "src/cocoa_platform.h",
+            "src/cocoa_joystick.h",
+            "src/posix_thread.h",
+
+
+            "src/cocoa_init.m",
+            "src/egl_context.m",
+            "src/nsgl_context.m",
+            "src/osmesa_context.m"
+
+            "src/cocoa_monitor.m",
+            "src/cocoa_window.m",
+            "src/cocoa_joystick.m",
+            "src/posix_thread.m",
+            "src/cocoa_time.m"
+        }
+
+        defines {
+            "_GLFW_COCOA"
         }
 
 	filter "configurations:Debug"
