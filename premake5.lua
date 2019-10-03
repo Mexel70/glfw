61ca83624a3cb11cafa3880f447277ded4a9f01a
Mexel70 <mexel70@live.de>
2019-10-03 17:08:52 +0200

    Fixed formatting

--- premake5.lua
+++ premake5.lua
@@ -40,8 +40,8 @@ project "glfw"
             "src/win32_window.c",
             "src/win32_joystick.c",
 
-            "src/win32_time.c",
             "src/win32_thread.c",
+            "src/win32_time.c"
         }
 
         defines { 
